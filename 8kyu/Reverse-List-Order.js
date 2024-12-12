fa1516e7f97601d568b3b9046ae29f4dea96465a
cziur <crisamziur@gmail.com>
2024-12-11 19:31:22 -0800

    added new challenge

---
+++ 8kyu/Reverse-List-Order.js
@@ -0,0 +1,5 @@
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+function reverseList(list) {
+  return list.reverse(); // Using the array method "reverse()" we can change the original array to reverse the order of the elements inside.
+}
