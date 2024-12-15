ed7ed9772d68329291a8c1ba2e916867b605a6d9
cziur <crisamziur@gmail.com>
2024-12-15 14:59:57 -0800

    added new challenge

---
+++ 8kyu/Even-or-Odd.js
@@ -0,0 +1,11 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers
+
+function evenOrOdd(number) {
+  if (number % 2 === 0) {
+    // Using the modulus operator, check for remainder
+    return "Even"; // if 0 left over, returns the string "Even"
+  } else {
+    // If not, returns the string "Odd"
+    return "Odd";
+  }
+}
