2d3c968bc91b9d582aab5e9701c53426e1266bf3
cziur <crisamziur@gmail.com>
2024-12-22 18:01:16 -0800

    added new challenge

---
+++ 8kyu/Drink-about.js
@@ -0,0 +1,32 @@
+// Kids drink toddy.
+// Teens drink coke.
+// Young adults drink beer.
+// Adults drink whisky.
+// Make a function that receive age, and return what they drink.
+
+// Rules:
+
+// Children under 14 old.
+// Teens under 18 old.
+// Young under 21 old.
+// Adults have 21 or more.
+
+
+function peopleWithAgeDrink(old) { 
+  if (old < 14) { 
+    return 'drink toddy'
+  } else if (old < 18) {
+    return 'drink coke'
+  } else if (old < 21) {
+    return 'drink beer'
+  } else if (old >= 21) {
+    return 'drink whisky'
+  }
+};
+
+// Another solution using ternary operation
+
+const peopleWithAgeDrink = (old) => 
+  old < 14 ? 'drink toddy' :
+  old < 18 ? 'drink coke' :
+  old < 21 ? 'drink beer' : 'drink whisky'
