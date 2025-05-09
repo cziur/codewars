e4dfbc4783937f48666550faa081e338a495a578
cziur <crisamziur@gmail.com>
2025-05-08 18:56:17 -0700

    Added solution

---
+++ 6kyu/Arraydiff.js
@@ -0,0 +1,7 @@
+// Implement a function that computes the difference between two lists.
+// The function should remove all occurrences of elements from the first list (a) that are present in the second list (b).
+// The order of elements in the first list should be preserved in the result.
+
+function arrayDiff(a, b) {
+	return a.filter((x) => !b.includes(x));
+}
